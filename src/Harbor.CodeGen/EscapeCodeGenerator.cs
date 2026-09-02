cc8628675eb464f258bbaadb33ddfa81fdf6ee63
Danil Chemezov <refusedguy@gmail.com>
2026-09-02 22:16:13 +0500

    chore: keep generated code + reports before chain restart

---
+++ src/Harbor.CodeGen/EscapeCodeGenerator.cs
@@ -0,0 +1,23 @@
+namespace Harbor.CodeGen
+
+using System.Text;
+
+/// <summary>Attribute to mark enums for escape code generation by EscapeCodeGenerator.</summary>
+[AttributeUsage(AttributeTargets.Enum, Inherited = false, AllowMultiple = false)]
+public sealed class TerminalEscapeAttribute : Attribute
+{
+    /// <summary>Initializes a new instance of the TerminalEscapeAttribute class.</summary>
+    public TerminalEscapeAttribute()
+    {
+    }
+}
+
+/// <summary>Attribute to mark enums for escape code generation by EscapeCodeGenerator.</summary>
+[AttributeUsage(AttributeTargets.Enum, Inherited = false, AllowMultiple = false)]
+public sealed class TerminalEscapeAttribute : Attribute
+{
+    /// <summary>Initializes a new instance of the TerminalEscapeAttribute class.</summary>
+    public TerminalEscapeAttribute()
+    {
+    }
+}
